e6fba7c4f647d784e31be23e5fce13d1f7220769
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2023-06-25 19:35:18 -0400

    Don't repeat session key verification logic.

--- src/location.rs
+++ src/location.rs
@@ -1,6 +1,7 @@
 use std::time::{Duration, Instant};
 
 use actix_web::{post, web, HttpResponse, Responder};
+use dashmap::DashMap;
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -39,42 +40,12 @@ pub(crate) async fn post_location_get(
     info: web::Json<LocationGetIn>,
     data: web::Data<AppState>,
 ) -> impl Responder {
-    // Don't bother reconstructing the durations each time, just keep them around.
-    static SHORT_EXPIRY: Duration = Duration::from_secs(SHORT_EXPIRY_SECS);
-    static LONG_EXPIRY: Duration = Duration::from_secs(LONG_EXPIRY_SECS);
     log::trace!(
         "/location/get: called with session key: {}",
         info.session_key
     );
-    // Try to get the session key from the table of allowed ones.
-    let expiry = {
-        match data.session_tokens.get(&info.session_key) {
-            None => {
-                log::debug!("/location/get: Bad session key.");
-                return forbidden();
-            }
-            Some(e) => TokenExpiry {
-                issued: e.issued,
-                last_used: e.last_used,
-            },
-        }
-    };
-    // Check if it's expired.
-    if expiry.issued.elapsed() > LONG_EXPIRY || expiry.last_used.elapsed() > SHORT_EXPIRY {
-        // If it is, remove it.
-        data.session_tokens.remove(&info.session_key);
-        log::debug!("/location/get: Expired session key.");
+    if !verify_session_key(info.session_key, &data.session_tokens) {
         return forbidden();
-    }
-    // We've gotten through authentication, update the token's last-used time.
-    {
-        data.session_tokens.insert(
-            info.session_key,
-            TokenExpiry {
-                last_used: Instant::now(),
-                issued: expiry.issued,
-            },
-        );
     }
     // Grab the last location measurement.
     let last_loc: Location = {
@@ -99,19 +70,30 @@ pub(crate) async fn post_location_list(
     info: web::Json<LocationListIn>,
     data: web::Data<AppState>,
 ) -> impl Responder {
-    // Don't bother reconstructing the durations each time, just keep them around.
-    static SHORT_EXPIRY: Duration = Duration::from_secs(SHORT_EXPIRY_SECS);
-    static LONG_EXPIRY: Duration = Duration::from_secs(LONG_EXPIRY_SECS);
     log::trace!(
         "/location/list: called with session key: {}",
         info.session_key
     );
+    if !verify_session_key(info.session_key, &data.session_tokens) {
+        return forbidden();
+    }
+    // Grab the list of names.
+    let names: Vec<String> = { data.last_location.lock().keys().cloned().collect() };
+    // Return our serialized data.
+    HttpResponse::Ok().body(serde_json::to_string(&names).unwrap())
+}
+
+fn verify_session_key(session_key: u128, session_tokens: &DashMap<u128, TokenExpiry>) -> bool {
+    // Don't bother reconstructing the durations each time, just keep them around.
+    static SHORT_EXPIRY: Duration = Duration::from_secs(SHORT_EXPIRY_SECS);
+    static LONG_EXPIRY: Duration = Duration::from_secs(LONG_EXPIRY_SECS);
+
     // Try to get the session key from the table of allowed ones.
     let expiry = {
-        match data.session_tokens.get(&info.session_key) {
+        match session_tokens.get(&session_key) {
             None => {
-                log::debug!("/location/list: Bad session key.");
-                return forbidden();
+                log::debug!("/location/*: Bad session key.");
+                return false;
             }
             Some(e) => TokenExpiry {
                 issued: e.issued,
@@ -122,24 +104,21 @@ pub(crate) async fn post_location_list(
     // Check if it's expired.
     if expiry.issued.elapsed() > LONG_EXPIRY || expiry.last_used.elapsed() > SHORT_EXPIRY {
         // If it is, remove it.
-        data.session_tokens.remove(&info.session_key);
-        log::debug!("/location/list: Expired session key.");
-        return forbidden();
+        session_tokens.remove(&session_key);
+        log::debug!("/location/*: Expired session key.");
+        return false;
     }
     // We've gotten through authentication, update the token's last-used time.
     {
-        data.session_tokens.insert(
-            info.session_key,
+        session_tokens.insert(
+            session_key,
             TokenExpiry {
                 last_used: Instant::now(),
                 issued: expiry.issued,
             },
         );
     }
-    // Grab the list of names.
-    let names: Vec<String> = { data.last_location.lock().keys().cloned().collect() };
-    // Return our serialized data.
-    HttpResponse::Ok().body(serde_json::to_string(&names).unwrap())
+    true
 }
 
 #[post("/location/update")]
