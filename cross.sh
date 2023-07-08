64a6796daafc22aef9fde93ada219b941d3b19d0
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2023-07-07 21:30:30 -0400

    Fix packaging when cross compiling.

--- cross.sh
+++ cross.sh
@@ -6,3 +6,4 @@ export ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_LIB_DIR="$SYSROOT/usr/lib/arm-linux
 export OPENSSL_INCLUDE_DIR="$SYSROOT/usr/include"
 export OPENSSL_LIB_DIR="$SYSROOT/usr/lib"
 cargo build --target=armv7-unknown-linux-gnueabihf --release
+cp target/{armv7-unknown-linux-gnueabihf/,}/release/locationapp-server
